3f54a96437c3b623180aaef75c697f13114317f7
luis rodriguez <lalrop92@gmail.com>
2018-01-29 11:45:15 -0300

    pegado en parte 3, parte 4 completa

--- prueba.rb
+++ prueba.rb
@@ -16,27 +16,49 @@ puts '     Seleccione una opcion de las siguientes:
 
 opcion = gets.chomp.to_i
 
-case opcion
-    when 1
-      for i in 0..(a.length-1)
-        alumno_i = a[i]
-        nombre = alumno_i[0]
-        notas = alumno_i[1..-1]
-        sumatoria = notas.inject(0){|sum,i| sum = sum + i.to_i}
-        promedio = sumatoria/notas.length
-        puts hash1 = {nombre => promedio}
-      end
-    when 2
-      for i in 0..(a.length-1)
-        alumno_i = a[i]
-        nombre = alumno_i[0]
-        inasistencias = alumno_i.count('A')
-        puts hash2 = {nombre => inasistencias}
-      end
-    when 3
-      
-      end
+while opcion !=4
+    case opcion
+      when 1
+        for i in 0..(a.length-1)
+          alumno_i = a[i]
+          nombre = alumno_i[0]
+          notas = alumno_i[1..-1]
+          sumatoria = notas.inject(0){|sum,i| sum = sum + i.to_i}
+          promedio = sumatoria/notas.length
+          puts hash1 = {nombre => promedio}
+        end
 
-    #
-    # when 3
+      when 2
+        for i in 0..(a.length-1)
+          alumno_i = a[i]
+          nombre = alumno_i[0]
+          inasistencias = alumno_i.count('A')
+          puts hash2 = {nombre => inasistencias}
+        end
+
+      when 3
+          def aprobacion(nota)
+            for i in 0..(a.length-1)
+              alumno_i = a[i]
+              nombre = alumno_i[0]
+              notas = alumno_i[1..-1]
+              sumatoria = notas.inject(0){|sum,i| sum = sum + i.to_i}
+              promedio = sumatoria/notas.length
+              hash1 = {nombre => promedio}
+              puts nota
+
+              puts aprobacion(5)
+            end
+          end
+    end
+
+puts '     Seleccione una opcion de las siguientes:
+          1 : Alumnos junto con su promedio
+          2 : Numero de inasistencias
+          3 : Alumnos aprobados
+          4 : Salir'
+
+opcion = gets.chomp.to_i
+
 end
+puts "Usted a salido, Adios!"
