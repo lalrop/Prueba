d36cba90724bc9ba9bd2de2d42fce9a30aff7efe
luis rodriguez <lalrop92@gmail.com>
2018-01-29 11:00:40 -0300

    parte 1 lista

---
+++ prueba.rb
@@ -0,0 +1,32 @@
+def read_alum(alumnos)
+file = File.open(alumnos, 'r')
+alum = file.readlines.map(&:chomp).map { |lines| lines.split(', ') }
+file.close
+alum
+end
+
+a = read_alum('alumnos.csv')
+print a
+
+puts '     Seleccione una opcion de las siguientes:
+      1 : Alumnos junto con su promedio
+      2 : Numero de inasistencias
+      3 : Alumnos aprobados
+      4 : Salir'
+
+opcion = gets.chomp.to_i
+
+case opcion
+    when 1
+      for i in 0..(a.length-1)
+        alumno_i = a[i]
+        nombre = alumno_i[0]
+        notas = alumno_i[1..-1]
+        sumatoria = notas.inject(0){|sum,i| sum = sum + i.to_i}
+        promedio = sumatoria/notas.length
+        puts hash1 = {nombre => promedio}
+      end
+    when 2
+      
+    # when 3
+end
